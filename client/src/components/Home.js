05b159bdcc35b4aed5140babef5a3f914d4d6112
Nisha <nisha0217@gmail.com>
2024-12-05 21:36:03 +0100

    Apolloclient Added

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,27 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
+import { GET_ALL_QUOTES } from "../gqloperations/queries";
 
 export default function Home() {
+  const { loading, error, data } = useQuery(GET_ALL_QUOTES);
+
+  if (loading) return <h1>Loading</h1>;
+  if (error) {
+    console.log(error.message);
+  }
+  if(data.quotes.length === 0){
+    return  <h2>No Quotes available</h2>
+   }
   return (
     <div className="container">
-      <blockquote>
-        <h6>if it works dont touch it</h6>
-        <p className="right-align">~ram</p>
-      </blockquote>
-      <blockquote>
-        <h6>if it works dont touch it</h6>
-        <p className="right-align">~ram</p>
-      </blockquote>
+      {data.quotes.map((quote, index) => {
+        return (
+          <blockquote key={index}>
+            <h6>{quote.name}</h6>
+            <p className="right-align">~{quote.by.firstName}</p>
+          </blockquote>
+        );
+      })}
     </div>
   );
 }
